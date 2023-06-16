c0530f57efc9d10aac860733ded396795923a0d6
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-06-16 15:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+const audio = new Audio();
+
+buttons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    const sound = this.dataset.sound;
+    audio.src = `sounds/${sound}`;
+    audio.play();
+  });
+});
+
+stopButton.addEventListener('click', function () {
+  audio.pause();
+  audio.currentTime = 0;
+});
